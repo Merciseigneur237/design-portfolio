d5c9be6d6da8795c48e8e310c022b4647c44032a
Romeo Feukeu <155496758+Merciseigneur237@users.noreply.github.com>
2025-01-30 15:26:43 -0600

    Add files via upload

---
+++ javasript.js
@@ -0,0 +1,12 @@
+var tablinks = document.getElementById("tab-links");
+    var tabcontents = document.getElementById("tab-contents");
+    function opentab(tabname){
+      for(tablinks of tablinks){
+        tablinks.classList.remove(active-link);
+      }
+
+      for(tabcontents of tabcontents){
+        tabcontents.classList.remove(active-tab);
+      }
+
+      }
